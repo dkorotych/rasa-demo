f4dab5ae2276bdde1a4aa8d390e6f6ba15003a7c
Dmitry Korotych <dkorotych@gmail.com>
2020-02-09 00:50:43 +0300

    Use Docker Swarm

--- swarm/prepare.sh
+++ swarm/prepare.sh
@@ -8,6 +8,11 @@ CURRENT_DIRECTORY=$(pwd)
 cd "$(dirname $0)/.."
 docker build --tag sara-ui --file swarm/Dockerfile-ui .
 docker build --tag sara-bot --file swarm/Dockerfile-bot .
+docker build --tag sara-bot-balancer --file swarm/Dockerfile-balancer ./swarm
 docker build --tag sara-actions .
 
 cd "$CURRENT_DIRECTORY"
+
+docker stack rm sara
+sleep 20
+docker stack deploy --compose-file docker-stack.yml sara
